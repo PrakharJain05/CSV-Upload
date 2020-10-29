284da7fd4e764a998c0ef15dd8f2204d7d74210a
Prakhar Jain <65168776+PrakharJain05@users.noreply.github.com>
2020-10-29 20:37:33 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCK-9FwWxLnUtkp0pYjjuoc5t3wnHS3lIg",
-  authDomain: "csv-uploads-44f19.firebaseapp.com",
-  databaseURL: "https://csv-uploads-44f19.firebaseio.com",
-  projectId: "csv-uploads-44f19",
-  storageBucket: "csv-uploads-44f19.appspot.com",
-  messagingSenderId: "1000898357242",
-  appId: "1:1000898357242:web:9f21efb1a13c6fb4f7e643",
+  //config
 };
 
 firebase.initializeApp(firebaseConfig);
